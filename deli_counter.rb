15ae7b3f5d332f4ec2a8da3b547896dec7fa2b22
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-11 12:56:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+var katzDeliLine = []; 
+function takeANumber (katzDeliLine, name){
+  var position = katzDeliLine.length+1;
+   katzDeliLine.push(name);
+return "Welcome, " + name +". You are number "+ position +" in line.";
+} 
+end 
+
+
+
+function nowServing (deliLine){
+var customer;
+if(deliLine.length === 0){
+  return "There is nobody waiting to be served!"
+}
+customer = deliLine.shift()
+return "Currently serving "+ customer + "."
+}
+
+
+
+function currentLine(katzDeliLine){
+  var message = "The line is currently:" 
+  if(katzDeliLine.length ===0) {
+    return "The line is currently empty.";
+  } else 
+   for ( var i = 0; i < katzDeliLine.length; i++) { 
+     message += " " + (i + 1) + ". " + katzDeliLine[i] + "," 
+   } 
+   return message.slice(0, message.length - 1) 
+} 
